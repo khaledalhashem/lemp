cc0bb7684b7994ca30f49866ce70527d8b84d85a
khaledalhashem <kmalhashem@gmail.com>
2017-10-15 11:42:13 +0300

    added module ngx_fancyindex-0.4.2

--- ngx_pagespeed.sh
+++ ngx_pagespeed.sh
@@ -32,28 +32,28 @@ yum install -y wget perl perl-devel perl-ExtUtils-Embed libxslt libxslt-devel li
 mkdir $srcdir && cd $srcdir
 
 # Nginx version 1.12.1
-wget -c --no-check-certificate http://nginx.org/download/$ngxver.tar.gz --tries=3 && tar -zxf $ngxver.tar.gz && rm -rf $ngxver.tar.gz
+wget -c http://nginx.org/download/$ngxver.tar.gz --tries=3 && tar -zxf $ngxver.tar.gz && rm -rf $ngxver.tar.gz
 
 # pagespeed version 1.12.34.2
-wget -c --no-check-certificate https://github.com/pagespeed/ngx_pagespeed/archive/v$nps.tar.gz --tries=3 && tar -zxf v$nps.tar.gz && rm -rf v$nps.tar.gz
+wget -c https://github.com/pagespeed/ngx_pagespeed/archive/v$nps.tar.gz --tries=3 && tar -zxf v$nps.tar.gz && rm -rf v$nps.tar.gz
 
 cd ngx_pagespeed-$nps/
 # psol version 1.12.34.2
-wget -c --no-check-certificate https://dl.google.com/dl/page-speed/psol/$nps_psol-x64.tar.gz --tries=3 && tar -zxf $nps_psol-x64.tar.gz && rm -rf $nps_psol-x64.tar.gz
+wget -c https://dl.google.com/dl/page-speed/psol/$nps_psol-x64.tar.gz --tries=3 && tar -zxf $nps_psol-x64.tar.gz && rm -rf $nps_psol-x64.tar.gz
 
 cd $srcdir
 
 # PCRE version 8.40
-wget -c --no-check-certificate https://ftp.pcre.org/pub/pcre/pcre-8.40.tar.gz --tries=3 && tar -xzf pcre-8.40.tar.gz
+wget -c https://ftp.pcre.org/pub/pcre/pcre-8.40.tar.gz --tries=3 && tar -xzf pcre-8.40.tar.gz
 
 # zlib version 1.2.11
-wget -c --no-check-certificate https://www.zlib.net/zlib-1.2.11.tar.gz --tries=3 && tar -xzf zlib-1.2.11.tar.gz
+wget -c https://www.zlib.net/zlib-1.2.11.tar.gz --tries=3 && tar -xzf zlib-1.2.11.tar.gz
 
 # OpenSSL version 1.1.0f
-wget -c --no-check-certificate https://www.openssl.org/source/openssl-1.1.0f.tar.gz --tries=3 && tar -xzf openssl-1.1.0f.tar.gz
+wget -c https://www.openssl.org/source/openssl-1.1.0f.tar.gz --tries=3 && tar -xzf openssl-1.1.0f.tar.gz
 
 # ngx_fancyindex 0.4.2
-wget -c --no-check-certificate https://github.com/aperezdc/ngx-fancyindex/archive/v0.4.2.tar.gz --tries=3 && tar -zxf v0.4.2.tar.gz
+wget -c https://github.com/aperezdc/ngx-fancyindex/archive/v0.4.2.tar.gz --tries=3 && tar -zxf v0.4.2.tar.gz
 
 rm -rf *.gz
 
@@ -122,24 +122,19 @@ ln -s /usr/lib64/nginx/modules /etc/nginx/modules
 
 useradd --system --home /var/cache/nginx --shell /sbin/nologin --comment "nginx user" --user-group nginx
 
-cd /etc/nginx
-mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-wget https://github.com/khaledalhashem/nginx_custom/raw/master/nginx.conf
+cd /etc/nginx && mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak && wget https://github.com/khaledalhashem/nginx_custom/raw/master/nginx.conf
 
-cd /usr/lib/systemd/system/
-wget https://raw.githubusercontent.com/khaledalhashem/nginx_custom/master/nginx.service
+cd /usr/lib/systemd/system/ && wget https://raw.githubusercontent.com/khaledalhashem/nginx_custom/master/nginx.service
 
-cd /etc/init.d
-wget https://github.com/khaledalhashem/nginx_custom/raw/master/nginx
-chmod +x /etc/init.d/nginx
+cd /etc/init.d && wget https://github.com/khaledalhashem/nginx_custom/raw/master/nginx && chmod +x /etc/init.d/nginx
 
 mkdir -p /var/cache/nginx && nginx -t
 
 systemctl start nginx.service && systemctl enable nginx.service
 
-rm /etc/nginx/koi-utf /etc/nginx/koi-win /etc/nginx/win-utf
+rm -rf /etc/nginx/koi-utf /etc/nginx/koi-win /etc/nginx/win-utf
 
-rm /etc/nginx/*.default
+rm -rf /etc/nginx/*.default
 
 mkdir -p /var/ngx_pagespeed_cache
 chown -R nobody:nobody /var/ngx_pagespeed_cache
@@ -157,4 +152,4 @@ rpm -Uvh http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum -y install yum-utils
 
 yum-config-manager --enable remi-php71
-yum -y install --exclude=httpd php php-fpm php-opcache
+yum -y install php php-fpm php-opcache
