ade62f038465655a80a86712af0a95c42b394c2f
khaledalhashem <kmalhashem@gmail.com>
2018-10-30 10:54:22 +0300

    updated

--- lemptest_centos.sh
+++ lemptest_centos.sh
@@ -359,11 +359,11 @@ echo "LEMP Auto Installer `date`"
   echo "*************************************************"
 
 cat <<EOF >> /etc/yum.repos.d/MariaDB.repo
-# MariaDB 10.1 CentOS repository list
+# MariaDB 10.3 CentOS repository list
 # http://downloads.mariadb.org/mariadb/repositories/
 [mariadb]
 name = MariaDB
-baseurl = http://yum.mariadb.org/10.1/centos7-amd64
+baseurl = http://yum.mariadb.org/10.3/centos7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
 EOF
